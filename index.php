112335d9e370c31eac6f1c18cefa6d9da87a99c2
zubkoveu <zubkoveu@me.com>
2022-05-26 11:50:54 +0300

    intermediate

--- index.php
+++ index.php
@@ -1,7 +1,15 @@
 <?php
 require_once 'yml.php';
+
 $yml_c = new yml('UTF-8');
-$yml_c->get_xml_header();
+
+$yml_c->add_currency('RUR', '1', 0);
+$yml_c->add_currency('USD', '77', 0);
+$yml_c->add_currency('EUR', '88', 0);
+
+$yml_c->add_category('Игрушки', 1, -1);
+$yml_c->add_category('Куклы', 101, 1);
+
 
 echo $yml_c->get_xml_header();
 echo 'test works';
