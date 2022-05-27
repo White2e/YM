682be0955b0824395b964c9827300cbb59d27816
zubkoveu <zubkoveu@me.com>
2022-05-27 12:39:35 +0300

    intermediate

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 require_once 'yml.php';
+var $offerdata = [];
 
 $yml_c = new yml('UTF-8');
 
@@ -10,6 +11,7 @@ $yml_c->add_currency('EUR', '88', 0);
 $yml_c->add_category('Игрушки', 1, -1);
 $yml_c->add_category('Куклы', 101, 1);
 
+$offerdata[] = 'https://ru.123rf.com/photo_60275032', '50.01', 'RUR', '101', '', '';
 
 echo $yml_c->get_xml_header();
 echo 'test works';
